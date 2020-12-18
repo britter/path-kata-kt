a0d559fabbb83bf53f5d779e3dd6327f1ae2b6bb
Benedikt Ritter <beneritter@gmail.com>
2020-12-18 10:25:25 +0100

    Project setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,23 +20,17 @@ repositories {
 }
 
 dependencies {
-    // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // This dependency is used by the application.
-    implementation("com.google.guava:guava:29.0-jre")
-
-    // Use the Kotlin test library.
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
 }
 
 application {
     // Define the main class for the application.
     mainClass.set("path.kata.kt.AppKt")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
